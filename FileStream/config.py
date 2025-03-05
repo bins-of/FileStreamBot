b73865db5bf380f561a5a9cf727e9e9b025e89ca
bins-of <binsofbins1@gmail.com>
2025-03-06 00:46:07 +0600

    Update config.py

--- FileStream/config.py
+++ FileStream/config.py
@@ -33,6 +33,4 @@ class Server:
     HAS_SSL = str(env.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(env.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(env.get("FQDN", "rare-rubina-rm-stream-bc538780.koyeb.app"))
-    URL = "http{}://{}{}/".format(
-        "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
-    )
+    URL = "http{}://{}/".format("s" if HAS_SSL else "", FQDN)
